de6ef6748181f6031f76226a6ed4cbbcda96a55e
dannyk-99 <dannykadri0@gmail.com>
2021-05-28 16:35:19 +0100

    Seperating methods Caller Handler As a Trait

--- src/Arabic.php
+++ src/Arabic.php
@@ -2,24 +2,21 @@
 
 namespace Adnane\Arabic;
 
-
 use Adnane\Arabic\Ar\Grammar;
+use Adnane\Arabic\Caller;
 
 class Arabic
 {
-    /* 
-    | ========================================================
-    | => Here where you can define new methods & refrence them 
-    | to theire containing instances , as they will be handled 
-    | & declared dynamicly by this instance.
-    |
-    | => you may override methods' names and name theme as you 
-    | or your project desire !
-    | ========================================================
+    use Caller;
+    /**
+     * Here where you can define new methods & refrence them
+     * to theire containing instances , as they will be handled 
+     * & declared dynamicly by this instance.
+     * you may override methods' names and name theme as you 
+     * or your project desire !
     */
     private static $methods = 
     [
-        #numbers and integers
         'toWords' => [ Ar\Tafkit::class ,'toWords' ],
         'fromWords' => [ Ar\Tafkit::class ,'fromWords' ],
         'toOrdinal'  => [ Ar\Tafkit::class ,'toOrdinal' ],
@@ -34,43 +31,4 @@ class Arabic
         'countWords' => [ Ar\Strings::class ,'utf8WordCount'],
         'containsAr' => [ Ar\Strings::class ,'containsArabic'],
     ];
-
-    /* 
-    | ========================================================
-    | Here where you we are loading & calling the methods using 
-    |
-    | the instance's private property $methods 
-    | method can be called staticly using __callStatic
-    | or non staticly using __call , as they both use the calls
-    | handler method
-    | ========================================================
-    */
-    # static 
-    public static function __callStatic($method ,$params)
-    {
-
-        return self::handleCall($method ,$params);
-    
-    }
-
-    # non static
-    function __call($method ,$params)
-    {
-
-        return self::handleCall($method ,$params);
-    
-    }
-    
-    # calls handler
-    private static function handleCall($method ,$params)
-    {
-        if(array_key_exists($method ,self::$methods))
-        {
-            $instance = self::$methods[$method][0];
-
-            $method = self::$methods[$method][1];
-            
-            return call_user_func_array([$instance , $method] ,$params);
-        }
-    }
 }
---
+++ src/Caller.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace Adnane\Arabic;
+
+trait Caller
+{
+    /** 
+     * call methods staticly 
+     * 
+     * @return void
+     * */
+    public static function __callStatic($method ,$params)
+    {
+        return self::handleCall($method ,$params);
+    }
+    
+    /** 
+     * call methods non staticly  
+     * 
+     * @return void
+     * */
+    function __call($method ,$params)
+    {
+        return self::handleCall($method ,$params);
+    }
+
+    /** 
+     * handle call  
+     * 
+     * @return void
+     * */
+    private static function handleCall($method ,$params)
+    {
+        if(array_key_exists($method ,self::$methods))
+        {
+            $instance = self::$methods[$method][0];
+            $method = self::$methods[$method][1];
+            
+            return call_user_func_array([$instance , $method] ,$params);
+        }
+    }
+}
